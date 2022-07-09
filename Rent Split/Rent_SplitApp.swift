4dc01c7db7fd03a107675c8290ea4189fc9f59da
Ky Leggiero <KyLeggiero@Gmail.com>
2022-07-09 00:57:26 -0600

    Initial Commit

---
+++ Rent Split/Rent_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rent_SplitApp.swift
+//  Rent Split
+//
+//  Created by S🌟System on 2022-07-07.
+//
+
+import SwiftUI
+
+@main
+struct Rent_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
